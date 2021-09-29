f9eb986c5eea5ec71d41a471f82dffd432cf6e34
Salfors <87897405+Salfors@users.noreply.github.com>
2021-09-29 04:38:02 -0500

    Add files via upload

---
+++ arch.sh
@@ -0,0 +1,54 @@
+echo -e "\nInstalling prereqs...\n"
+pacman -S --noconfirm gptfdisk btrfs-progs
+
+echo "-------------------------------------------------"
+echo "-------select your disk to format----------------"
+echo "-------------------------------------------------"
+lsblk
+echo "Please enter disk: (example /dev/sda)"
+read DISK
+echo "--------------------------------------"
+echo -e "\nFormatting disk...\n"
+echo "--------------------------------------"
+echo -e "\nNote: Enter values in MB on next step will input be like that =58M"
+# disk prep
+sgdisk -Z ${DISK} # zap all on disk
+sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+
+# create partitions
+sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
+echo "Please enter size for root partition"
+read RooP
+
+sgdisk -n 2:0:"+"$RooP""   ${DISK} # partition 2 (Root), default start, remaining
+
+echo -e "\nPlease do you want create home part or not "
+read  -p "your answer " answer
+if [ $answer == "yes" ]
+then
+    echo -e "\nEnter your /home partition size please"
+    read Homep   
+    echo sgdisk -n 3:0:"+"$Homep"" ${DISK} #partition 3 (/home)
+    echo -e 
+
+else 
+ 
+
+    echo "Ok no problem " 
+  
+fi
+echo -e "\nPlease did you want create swap part or not"
+read  -p "your answer " answer2
+if [ $answer2 == "yes" ]
+then
+    echo -e "\nEnter your Swap partition size please"
+    read Swap
+    echo sgdisk -n 4:0:"+"$Swap"" ${DISK} #partition 4 (Swap)
+    echo -e 
+
+else 
+ 
+
+    echo "Ok no problem " 
+  
+fi
