03b83e90c1a7a300870d05742846b7667f92e15c
Salfors <87897405+Salfors@users.noreply.github.com>
2022-03-17 11:01:58 -0700

    Update yeah.sh

--- yeah.sh
+++ yeah.sh
@@ -206,12 +206,14 @@ function Check_Hard_Disk_Status() {
               # for 3 or more  part 
               esac
 
-            break  # for break WHILE 0      
+              break  # for break WHILE 0      
 
             else
+
               echo -e "\n${SM} All Primary Partitions Are In Use. ${EM}\n"
               EDIT_HARD_DISK
               Check_Hard_Disk_Status
+              
             fi
 
 
